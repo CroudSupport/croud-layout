c088db82cb9f928036f7dd41040ea83a73ccb42e
Brock Reece <brockreece@gmail.com>
2017-09-15 10:52:44 +0200

    Change to import statement

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueSemantic from 'croud-vue-semantic'
+
 import App from './App'
 import store from './store'
 import axios from './axios'
@@ -9,7 +11,7 @@ import '../semantic/dist/semantic.min'
 import '../semantic/dist/semantic.min.css'
 
 Vue.config.productionTip = false
-Vue.use(require('croud-vue-semantic'))
+Vue.use(VueSemantic)
 
 /* eslint-disable no-new */
 new Vue({
